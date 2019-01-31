82d95f0c5bb2104255038c249f139ecf0b78e96b
Veronique Majo <vmajo02@gmail.com>
2019-01-31 11:29:06 +0100

    31 janvier

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-let x = 'hallo je kan je basis operaties hier doen. let go!!!'
-alert(x)
+const crel = require('crel')
+function renderCalculator()
+{
+    return crel ('main',renderScreen(), renderButtons())
+}
+function renderInputButton(data)
+{
+    const button = crel('button', data)
+    button.addEventListener('click', function()
+    {
+        const screen = document.getElementById('screen')
+        screen.value = screen.value+ data
+    })
+    return button
+}
+function renderEvaluateButton()
+{
+    return crel ('button', 'evaluate')
+}
+function renderButtons()
+{
+    const buttons = ['1','2','3','4','5','6','7','8','9','+','-','*','/','+-','C'].map(renderInputButton())
+    return crel('div', buttons, renderEvaluateButton())
+}
+function renderScreen()
+{
+    return crel ('input', {id: "screen"})
+}
+document.getElementById('root').appendChild(renderCalculator())
