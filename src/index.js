a19af22963215a95379aea0d5638a3822be30f23
Veronique Majo <vmajo02@gmail.com>
2019-01-31 14:15:07 +0100

    14h14 jeudi 31

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function renderEvaluateButton()
 }
 function renderButtons()
 {
-    const buttons = ['1','2','3','4','5','6','7','8','9','+','-','*','/','+-','C'].map(renderInputButton())
+    const buttons = "0123456789+-*/".split('').map(renderInputButton())
     return crel('div', buttons, renderEvaluateButton())
 }
 function renderScreen()
